5ff8d1d960660b80178cff73bf7cc7602596dda4
Alain <alain.dossche@vdabcampus.be>
2023-03-10 11:45:26 +0100

    start

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -1,18 +1,19 @@
 package be.vdab;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import be.vdab.repositories.PlantRepository;
+
 import java.sql.SQLException;
 
 public class Main {
-    private static final String URL = "jdbc:mysql://localhost/tuincentrum";
-    private static final String USER = "cursist";
-    private static final String PASSWORD = "cursist";
+
     public static void main(String[] args) {
-        try (var connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
-            System.out.println("Connectie geopend");
+        var repository = new PlantRepository();
+        try {
+            System.out.print(repository.verhoogPrijzenMet10Procent());
+            System.out.println(" planten aangepast.");
         } catch (SQLException ex) {
             ex.printStackTrace(System.err);
         }
     }
+
 }
